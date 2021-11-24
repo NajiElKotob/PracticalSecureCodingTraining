76ce12b9f22e415a52d451a8c1b62e8e4b67ed35
najielkotob <naji@dotnetheroes.com>
2021-11-24 10:20:20 +0200

    Add project files.

---
+++ PracticalSecureCodingTraining/Classes/StrongPassword.cs
@@ -0,0 +1,50 @@
+﻿namespace PracticalSecureCodingTraining.Classes;
+
+public static class StrongPassword
+{
+    public static bool ValidateStrongPassword(string password, int minLength = 8)
+    {
+        // Set all checks to false
+        var isUpper = false;
+        var isLower = false;
+        var isNumber = false;
+        var isSpecialCharacters = false;
+        var isMinimumLength = false;
+
+        const string specialCharacters = @"!@#$%^&*()[]{}<>_-=+/\|'`~;,.";
+
+        // Validate all conditions
+        if (password.Any(char.IsUpper))
+        {
+            isUpper = true;
+        }
+        if (password.Any(char.IsLower))
+        {
+            isLower = true;
+        }
+        if (password.Any(char.IsNumber))
+        {
+            isNumber = true;
+        }
+        if (password.IndexOfAny(specialCharacters.ToCharArray()) != -1)
+        {
+            isSpecialCharacters = true;
+        }
+        if (password.Length >= minLength)
+        {
+            isMinimumLength = true;
+        }
+        
+        //Validate strong password
+        if (isUpper && isLower && isNumber && isSpecialCharacters && isMinimumLength)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+
